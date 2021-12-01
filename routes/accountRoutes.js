7f98b53f91d4855856249d39d03e39bcf2138def
pademss <fatma.putri@ui.ac.id>
2021-12-01 22:13:18 +0700

    semangat aku

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -12,6 +12,6 @@ router
   .delete(controller.deleteAccount)
   .put(upload.single("image"), controller.updateAccount);
 
-router.route("/byemail").post(controller.getAccountByEmail);
+router.route("/byemail").get(controller.getAccountByEmail);
 
 module.exports = router;
